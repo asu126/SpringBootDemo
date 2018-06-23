b271e117a449e77604f499ffe86382cfe78c0342
asu <suguojun126@163.com>
2018-06-23 09:08:00 +0800

    init

---
+++ src/main/java/com/demo/mavendemo/MavenDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.mavendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDemoApplication.class, args);
+	}
+}
